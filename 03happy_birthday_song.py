3ad0a77ca608c382b4d4f270458cc4f75d063619
JubindraKc <juvin1997@gmail.com>
2017-11-28 19:40:53 +0545

    This is the latest assignment from Nov 24, 2017

---
+++ 03happy_birthday_song.py
@@ -0,0 +1,16 @@
+#03. Happy Birthday Song for a Friend
+
+name = input("What is your name?\n")
+
+print("So, Is it your birthday today?\n")
+
+ans = input("yes or no?\n")
+
+if(ans == 'yes'):
+
+
+    print("Happy Birthday to",name)
+    print("Happy Birthday to You\nMay God Bless You\nCheers!",name)
+
+else:
+    print("Never Mind Then...")
