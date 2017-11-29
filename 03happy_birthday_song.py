d64aacb7fc2639156536da72cfe2da74481da24a
JubindraKc <juvin1997@gmail.com>
2017-11-29 11:43:25 +0545

    Update 03happy_birthday_song.py

--- 03happy_birthday_song.py
+++ 03happy_birthday_song.py
@@ -1,22 +1,23 @@
 #03. Happy Birthday Song for a Friend
 
-def yes():
-    print("Happy Birthday to",name)
-    print("Happy Birthday to You\nMay God Bless You\nCheers!",name)
+def start():
+    
+    name = input("What is your name?\n")
+    print("So, Is it your birthday today?\n")
 
-def no():
+    ans = input("yes or no?\n")
+    if(ans == "yes"):
+        yes(name)
+    elif(ans == "no"):
+        no(name)
+
+
+
+def yes(name):
+    print("Happy Birthday to",name)
+    print("Happy Birthday to You\nMay God Bless You\nCheers! ",name)
+
+def no(name):
     print("Never Mind Then...")
 
-    
-
-name = input("What is your name?\n")
-
-print("So, Is it your birthday today?\n")
-
-ans = input("yes or no?\n")
-
-if(ans == 'yes'):
-    yes()
-
-else:
-    no()
+start()
